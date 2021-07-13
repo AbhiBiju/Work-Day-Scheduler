f97e833f842320d50ef0ac9d92810aacc7c574f3
AbhiBiju <abhinavbiju29@gmail.com>
2021-07-13 13:15:04 -0400

    initial psuedo code

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+// Current Date
+//Get current time with moment()
+// Format to Day, Month DateOrdinal
+// Update currentDay p with date
+
+// Time Blocks 9am-5pm (9 10 11 12 1 2 3 4 5)
+//Text Input for Time Blocks (Text Boxes inside table)
+//Validation (Cant edit past events/time blocks)
+
+// Past Present Future Color Coding
+//if(blockTime < currTime) grey color
+// else if(blockTime === currTime) red color
+// else if(blockTime > currTime) green color
+
+// Save Text in Time Blocks with Local Storage
+// use document.on(click .timeBlock)
+//Set textbox value to a variable
+// add new value to list
+// save to localStorage
